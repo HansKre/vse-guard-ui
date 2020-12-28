65422223d6e7500b337b42e326bdad866d75e818
Hans <hans.krebs@daimler.com>
2020-12-28 21:06:11 +0100

    Add tooltip to pie chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -9,6 +9,7 @@ const pieChart = (chartDefinition) => {
         <PieChart width={400} height={400}>
             <Pie data={chartDefinition.data.innerPie} dataKey="value" cx={200} cy={200} outerRadius={60} fill="#8884d8" />
             <Pie data={chartDefinition.data.outerPie} dataKey="value" cx={200} cy={200} innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+            <Tooltip />
         </PieChart>
     );
 }
