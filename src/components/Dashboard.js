fdb2f238a5b6267198cb5855ea941fc38d35f1d1
Hans <hans.krebs@daimler.com>
2021-01-01 16:08:23 +0100

    Add Error Handling and Loading Indication

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,11 +4,20 @@ import Grid from '@material-ui/core/Grid';
 import Section from './Section';
 import { useQuery } from '@apollo/react-hooks';
 import Queries from '../queries/queries';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+}));
 
 const Dashboard = () => {
-
-    // const { loading, error, data } = useQuery(Queries.GET_WEATHER);
-    // const q2 = useQuery(Queries.GET_WEATHER_FOR, { variables: { name: 'Sindelfingen' } });
+    const classes = useStyles();
     const { loading, error, data } = useQuery(Queries.GET_APPS);
 
     const extractSections = (apps) => {
@@ -19,15 +28,28 @@ const Dashboard = () => {
         return sections;
     }
 
-    return (
-        <Grid item xs={12} container spacing={2} justify='center'>
-            {data && extractSections(data.apps).map(section => {
-                return (
-                    <Section key={uuidv4()} section={section} apps={data.apps} />
-                )
-            })}
-        </Grid>
-    )
+    if (loading) {
+        return (
+            <Backdrop className={classes.backdrop} open={loading}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        )
+    } else if (error) {
+        console.log(error);
+        return (
+            <Typography key={uuidv4()}>{error.message}</Typography>
+        )
+    } else {
+        return (
+            <Grid item xs={12} container spacing={2} justify='center'>
+                {data && extractSections(data.apps).map(section => {
+                    return (
+                        <Section key={uuidv4()} section={section} apps={data.apps} />
+                    )
+                })}
+            </Grid>
+        )
+    }
 }
 
 export default Dashboard;
